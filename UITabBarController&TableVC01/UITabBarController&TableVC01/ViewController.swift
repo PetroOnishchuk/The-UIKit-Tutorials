6385efff202715bb2931073fbb38b62a1f6cdbf8
PetroOnishchuk <petroonishchuk@icloud.com>
2021-07-08 18:19:19 +0300

    Created UITabBarController&TableVC01 project

---
+++ UITabBarController&TableVC01/UITabBarController&TableVC01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITabBarController&TableVC01
+//
+//  Created by Petro Onishchuk on 7/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
