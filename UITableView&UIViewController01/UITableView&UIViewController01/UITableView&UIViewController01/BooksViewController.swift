18c2848c079107f6ff5733d6dfe135e20d059891
PetroOnishchuk <petroonishchuk@icloud.com>
2022-01-09 16:25:12 +0200

    Create func setupBooksTableView and also conform BookViewController to UITableView Data Source and Delegate

--- UITableView&UIViewController01/UITableView&UIViewController01/UITableView&UIViewController01/BooksViewController.swift
+++ UITableView&UIViewController01/UITableView&UIViewController01/UITableView&UIViewController01/BooksViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class BooksViewController: UIViewController {
+    
+    var booksTableView: UITableView!
+    
+    let numbers = [1, 3, 5, 10, 9, 11, 20]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      //  view.backgroundColor = .red
         // Do any additional setup after loading the view.
+        
+        title = "UITableView & UIViewController"
+        setupBooksTableView()
     }
+    
+    
+    
+    func setupBooksTableView() {
+        booksTableView = UITableView()
+        booksTableView.dataSource = self
+        booksTableView.delegate = self
+        
+        booksTableView.register(UITableViewCell.self, forCellReuseIdentifier: "bookCell")
+        
+        booksTableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(booksTableView)
+        
+        booksTableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        booksTableView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        booksTableView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        booksTableView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+    }
+    
+    
+    
     
 
     /*
@@ -26,4 +54,26 @@ class BooksViewController: UIViewController {
     }
     */
 
+}
+
+
+extension BooksViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numbers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = booksTableView.dequeueReusableCell(withIdentifier: "bookCell", for: indexPath)
+        
+        let number = numbers[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = "\(number)"
+        content.image = UIImage(systemName: "book")
+        
+        cell.contentConfiguration = content
+        return cell
+    }
 }
