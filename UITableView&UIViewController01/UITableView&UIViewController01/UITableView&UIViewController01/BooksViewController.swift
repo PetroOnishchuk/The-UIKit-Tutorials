da5633ecd237728c871ef3d4093c743ab64c59b9
PetroOnishchuk <petroonishchuk@icloud.com>
2022-01-09 14:52:45 +0200

    Create BooksViewController

---
+++ UITableView&UIViewController01/UITableView&UIViewController01/UITableView&UIViewController01/BooksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BooksViewController.swift
+//  UITableView&UIViewController01
+//
+//  Created by Petro Onishchuk on 1/9/22.
+//
+
+import UIKit
+
+class BooksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
