97b6e6dfb390f8bd4bf0f9ba0196f48ed93cd5fa
PetroOnishchuk <petroonishchuk@icloud.com>
2022-01-09 18:06:34 +0200

    Create book.swift model

---
+++ UITableView&UIViewController01/UITableView&UIViewController01/UITableView&UIViewController01/Model/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  UITableView&UIViewController01
+//
+//  Created by Petro Onishchuk on 1/9/22.
+//
+
+import UIKit
+
+struct Book {
+    let title: String
+    let author: String
+    let publicationDate: String
+}
